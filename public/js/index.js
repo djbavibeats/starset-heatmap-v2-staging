a0def19516f28fcd17a63f7667975805e6087c48
djbavibeats <justin@voltcreative.com>
2021-08-26 15:28:01 -0400

    Make the dots big!!

--- public/js/index.js
+++ public/js/index.js
@@ -130,37 +130,35 @@ function renderMap() {
                     'source': 'BMIS',
                     'minzoom': 14,
                     'paint': {
-                        'circle-radius': {
-                            'property': 'dbh',
-                            'type': 'exponential',
-                            'stops': [
-                                [{ zoom: 15, value: 1 }, 5],
-                                [{ zoom: 15, value: 62 }, 10],
-                                [{ zoom: 22, value: 1 }, 20],
-                                [{ zoom: 22, value: 62 }, 50]
-                            ]
-                        },
-                        'circle-color': {
-                            'property': 'dbh',
-                            'type': 'exponential',
-                            'stops': [
-                                [0, 'rgba(236,222,239,0)'],
-                                [10, 'rgb(236,222,239)'],
-                                [20, 'rgb(208,209,230)'],
-                                [30, 'rgb(166,189,219)'],
-                                [40, 'rgb(103,169,207)'],
-                                [50, 'rgb(28,144,153)'],
-                                [60, 'rgb(1,108,89)']
-                            ]
-                        },
-                        'circle-stroke-color': 'white',
-                        'circle-stroke-width': 1,
-                        'circle-opacity': {
-                            'stops': [
-                                [14, 0],
-                                [15, 1]
-                            ]
-                        }
+                    // increase the radius of the circle as the zoom level and dbh value increases
+                    'circle-radius': {
+                        'base': 1.35,
+                        'stops': [
+                            [12, 2],
+                            [22, 180]
+                        ]
+                    },
+                    'circle-color': {
+                    'property': 'dbh',
+                    'type': 'exponential',
+                    'stops': [
+                    [0, 'rgba(236,222,239,0)'],
+                    [10, 'rgb(236,222,239)'],
+                    [20, 'rgb(208,209,230)'],
+                    [30, 'rgb(166,189,219)'],
+                    [40, 'rgb(103,169,207)'],
+                    [50, 'rgb(28,144,153)'],
+                    [60, 'rgb(1,108,89)']
+                    ]
+                    },
+                    'circle-stroke-color': 'white',
+                    'circle-stroke-width': 1,
+                    'circle-opacity': {
+                    'stops': [
+                    [14, 0],
+                    [15, 1]
+                    ]
+                    }
                     }
             }, 'waterway-label');
             map.on('click', 'trees-point', function(e) {
